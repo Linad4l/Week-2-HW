aadf21594c76038f4ab435803cf174c269f64e09
Linad4l <146023098+Linad4l@users.noreply.github.com>
2026-02-01 19:54:04 -0800

    Update week1_program1.cpp

--- week1_program1.cpp
+++ week1_program1.cpp
@@ -1,1 +1,17 @@
 
+// week1_program1.cpp
+// Program 1: Sum of Two Numbers
+// Stores 50 and 100 in variables and stores their sum in a variable named total.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 50;     // first integer
+    int num2 = 100;    // second integer
+    int total = num1 + num2;  // sum of num1 and num2
+
+    cout << "The total is: " << total << endl;
+
+    return 0;
+}
