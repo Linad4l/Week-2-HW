3a55e1ff2dd3b157b78281e10bfe17fa0132c31a
Linad4l <146023098+Linad4l@users.noreply.github.com>
2026-02-01 20:02:35 -0800

    Update week1_program3.cpp

--- week1_program3.cpp
+++ week1_program3.cpp
@@ -1,1 +1,16 @@
 
+// week1_program3.cpp
+// Program 3: Triangle Pattern
+// Displays a simple triangle pattern using stars.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "   *" << endl;
+    cout << "  ***" << endl;
+    cout << " *****" << endl;
+    cout << "*******" << endl;
+
+    return 0;
+}
