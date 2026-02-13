e3126be14a12cdf42e0fd3350ad5b66c7e5aeb5c
Linad4l <146023098+Linad4l@users.noreply.github.com>
2026-02-13 02:01:35 -0800

    Update wk4_program1.cpp

--- wk4_program1.cpp
+++ wk4_program1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int num1,num2;
+    cout << "Enter number 1: ";
+    cin>>num1;
+    cout << "Enter number 2: ";
+    cin>>num2;
+    int larger = (num1>num2) ? num1:num2;
+    int smaller = (num2>num1) ? num1: num2;
+    
+    if (num1==num2)
+    {
+        cout << "The numbers are equal" <<endl;
+    }
+    else
+    {
+        cout << larger << " is larger" << endl;
+        cout << smaller << " is smaller" << endl;
+    }
+    return 0;
+}
